1975d49a04d424eb4d74e87b83b82208056481fa
kleinercalle <kleiner2024@gmail.com>
2024-07-25 01:14:15 +0000

    editando

--- src/js/app.js
+++ src/js/app.js
@@ -30,17 +30,27 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  document.querySelector("#widget_content").innerHTML = `
+  <div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+          <h1>${variables.name ? variables.name : "alejandra"} ${
+    variables.lastName ? variables.lastname : "garcia"
+  } </h1>
+          <h2> ${variables.role ? variables.role : "Web Developer"}</h2>
+          <h3> ${variables.city ? variables.city : "caracas"} ${
+    variables.country ? variables.country : "venezuela"
+  }</h3>          <ul class= ${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twiter ? variables.twiter : "4GeeksAcademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4GeeksAcademy"
+            }"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4GeeksAcademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
